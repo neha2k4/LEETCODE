71ab3b4a09c8741224dff3f21497ea15af223f8c
Neha <118845830+neha2k4@users.noreply.github.com>
2025-01-04 09:23:49 +0530

    Time: 0 ms (100%), Space: 23.8 MB (33.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,7 +4,8 @@ public:
        int j=0;
        for(int i=0;i<nums.size();i++){
         if(nums[i]!=0){
-            nums[j++]=nums[i];
+            nums[j]=nums[i];
+            j++;
          
         }
        }
