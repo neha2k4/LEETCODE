79549d71580ef4bd56feae9673f3b452fb0e0edd
Neha <118845830+neha2k4@users.noreply.github.com>
2025-02-07 23:19:42 +0530

    Time: 161 ms (38.91%), Space: 168 MB (30.23%) - LeetHub

--- 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -1,51 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
-        unordered_map<int, int> ballColors;  // Stores ball -> color mapping
-        unordered_map<int, int> colorFreq;   // Stores color -> frequency
-        vector<int> result; // Stores the number of distinct colors after each query
+        unordered_map<int, int> b2c;
+        unordered_map<int, int> freq;
+        unordered_set<int> clr;
+        vector<int> res;
 
-        for(auto& query : queries) {
-            int ball = query[0], color = query[1];
+        for (auto& q : queries) {
+            int ball = q[0], color = q[1];
 
-            // If the ball already has a color, update frequency
-            if(ballColors.count(ball)) {
-                int oldColor = ballColors[ball];
-                colorFreq[oldColor]--;
-
-                if(colorFreq[oldColor] == 0) {
-                    colorFreq.erase(oldColor);
-                }
+            if (b2c.count(ball)) {
+                int prevColor = b2c[ball];
+                if (--freq[prevColor] == 0) clr.erase(prevColor);
             }
 
-            // Assign the new color
-            ballColors[ball] = color;
-            colorFreq[color]++;
-
-            // Store the count of distinct colors
-            result.push_back(colorFreq.size());
+            b2c[ball] = color;
+            freq[color]++;
+            clr.insert(color);
+            res.push_back(clr.size());
         }
 
-        return result;
+        return res;
     }
 };
-
-// Example Usage
-int main() {
-    Solution sol;
-    vector<vector<int>> queries = {{0,1}, {1,4}, {1,1}, {1,4}, {1,1}};
-    vector<int> result = sol.queryResults(1, queries);
-
-    for(int val : result) {
-        cout << val << " ";
-    }
-    cout << endl;  // Expected Output: 1 2 1 2 1
-
-    return 0;
-}
