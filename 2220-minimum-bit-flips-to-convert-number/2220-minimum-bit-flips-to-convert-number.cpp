868502d845cb1259f232d3ca8d335c2cbbcf3503
Neha <118845830+neha2k4@users.noreply.github.com>
2025-03-15 17:56:36 +0530

    Time: 0 ms (100%), Space: 8.1 MB (25.81%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
-    int minBitFlips(int s, int g) {
-        return __builtin_popcount(s ^ g);
+    int minBitFlips(int start, int goal) {
+        bitset<32> binary1(start);
+        bitset<32> binary2(goal);
+
+        string sx = binary1.to_string();
+        string gy = binary2.to_string();
+
+        if (sx == gy)
+            return 0;
+
+        int count = 0;
+        for (int i = 31; i >= 0; i--) {
+
+            if (sx[i] != gy[i]) {
+                count += 1;
+                if (sx[i] == '0') {
+                    sx[i] = '1';
+                } else if (sx[i] == '1') {
+                    sx[i] = '0';
+                }
+            }
+
+            if (sx == gy)
+                return count;
+        }
+        return -1;
     }
 };
