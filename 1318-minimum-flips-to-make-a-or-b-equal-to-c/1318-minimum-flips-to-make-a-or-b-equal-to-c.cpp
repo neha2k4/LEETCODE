fa1172fb4862910cc70715d1597ed774024e2824
Neha <118845830+neha2k4@users.noreply.github.com>
2025-03-15 17:55:08 +0530

    Time: 3 ms (25.3%), Space: 7.7 MB (90.85%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int flips = 0;
-        while (a > 0 || b > 0 || c > 0) {
-            int bitA = a & 1;
-            int bitB = b & 1;
-            int bitC = c & 1;
-
-            if (bitC == 0) {
-                flips += (bitA + bitB); 
-            } else {
-                if (bitA == 0 && bitB == 0) {
-                    flips += 1; 
-                }
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((c & (1 << i)) == 0) { 
+                // If c has 0 at this bit, check a and b
+                if ((a & (1 << i)) && (b & (1 << i))) 
+                    ans += 2;  // If both a and b have 1, we need to flip both
+                else if ((a & (1 << i)) || (b & (1 << i))) 
+                    ans += 1;  // If either a or b has 1, we need to flip one
+            } else { 
+                // If c has 1 at this bit, at least one of a or b should be 1
+                if (!(a & (1 << i)) && !(b & (1 << i))) 
+                    ans += 1;  // If both are 0, we need one flip
             }
-
-            a >>= 1;
-            b >>= 1;
-            c >>= 1;
         }
-
-        return flips;
+        return ans;
     }
 };
