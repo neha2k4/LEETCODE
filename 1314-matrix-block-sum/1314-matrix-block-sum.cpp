73578c1ef4dddefd20d5aac699482026cdbc0052
Neha <118845830+neha2k4@users.noreply.github.com>
2025-03-15 17:42:48 +0530

    Time: 165 ms (19.09%), Space: 13.5 MB (96.91%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -1,44 +1,34 @@
-class Solution {
+class Solution
+{
 public:
- vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        vector<vector<int>> answer(mat.size(), vector<int>(mat[0].size()));
-        for(int i = 0; i < mat.size(); i++) {
-            for(int j = 0; j < mat[0].size(); j++) {
-                int rowStart = i-k;
-                int rowEnd = i+k;
-                int colStart = j-k;
-                int colEnd = j+k;
-
-                if(rowStart < 0) {
-                    rowStart = 0;
+    vector<vector<int>> matrixBlockSum(vector<vector<int>> &mat, int k)
+    {
+        int r = mat.size();
+        int c = mat[0].size();
+        vector<vector<int>> ans(r, vector<int>(c, 0));
+        for (int i = 0; i < r; ++i)
+        {
+            for (int j = 0; j < c; ++j)
+            {
+                int val = 0;
+                int minrow = i - k;
+                int maxrow = i + k;
+                int mincol = j - k;
+                int maxcol = j + k;
+                while (minrow < 0) ++minrow;
+                while (maxrow > (r - 1)) --maxrow;
+                while (mincol < 0) ++mincol;
+                while (maxcol > (c - 1)) --maxcol;
+                for (int p = minrow; p <= maxrow; ++p)
+                {
+                    for (int q = mincol; q <= maxcol; ++q)
+                    {
+                        val += mat[p][q];
+                    }
                 }
-
-                if(rowEnd >= mat.size()) {
-                    rowEnd = mat.size() - 1;
-                }
-
-                if(colStart < 0) {
-                    colStart = 0;
-                }
-
-                if(colEnd >= mat[0].size()) {
-                    colEnd = mat[0].size() - 1;
-                }
-
-                answer[i][j] = blockSum(mat, rowStart, rowEnd, colStart, colEnd);
+                ans[i][j] = val;
             }
         }
-
-        return answer;
-    }
-    int blockSum(vector<vector<int>>& mat, int rowStart, int rowEnd, int colStart, int colEnd) {
-        int sum = 0;
-
-        for(int i = rowStart; i <= rowEnd; i++) {
-            for(int j = colStart; j <= colEnd; j++) {
-                sum += mat[i][j];
-            }
-        }
-        return sum;
+        return ans;
     }
 };
