a0798f17c6e2bd67d7001484d48a908084d17e66
Neha <118845830+neha2k4@users.noreply.github.com>
2025-03-15 18:08:11 +0530

    Time: 10 ms (66.99%), Space: 76.7 MB (46.6%) - LeetHub

--- 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.cpp
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.cpp
@@ -1,64 +1,52 @@
 class Solution {
 public:
-    // First of all check lcp matrix is valid or not? If not return empty string
-    bool isLcpValid(vector<vector<int>>&lcp)
-    {
-        /*
-            observation-1: lcp[0][0]=dimesnion of matrix;
-            observation-2: lcp[i][j]=lcp[j][i] as in both case two strings [i to n-1] and [j to n-1] are same.
-            observation-3: lcp[i+1][j+1]=lcp[i][j]-1 if lcp[i][j]>0
-            observation-4: lcp[i][j]<=n- max(i,j)
-        */
-        int n=lcp.size();
-        if(lcp[0][0]!=n) return false;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-               if(lcp[i][j]!=lcp[j][i]) return false;
-               if(lcp[i][j]>n-max(i,j)) return false;
-               if(i+1<n && j+1<n && lcp[i][j]>0 &&  lcp[i+1][j+1]!=lcp[i][j]-1) return false;
-            }
-        }
-        return true;
-    }
-    string findTheString(vector<vector<int>>& lcp) 
-    {
-        if(isLcpValid(lcp)==false)
-        {
-          // invalid LCP
-            return "";
-        } 
-        int n=lcp.size();
-        char currChar='a';
-        vector<char>v(n,'#');
-        for(int i=0;i<n;i++)
-        {
-            if(v[i]!='#')continue;
-            if(currChar>'z') 
-            {
-               // Characters exceeded
-                return "";
-            }
-            for(int j=0;j<n;j++)
-            {
-                if(lcp[i][j]>0) v[j]=currChar; // if two substrings have lcp>0, means have some common prefix, so s[j]=s[i]
-            }
-             currChar++;
-        }
-        string ans="";
-        for(auto it:v)ans+=it;
-        //check if my builded string is satisfying the given lcp or not.
-        for (int i = 0; i < n; ++i) 
-        {
-            for (int j = 0; j < n; ++j) 
-            {
-                int v = i + 1 < n && j + 1 < n ? lcp[i + 1][j + 1] : 0;
-                v = ans[i] == ans[j] ? v + 1 : 0;
-                if (lcp[i][j] != v) return "";
+    string findTheString(vector<vector<int>>& lcp) {
+        int n = lcp.size();
+        // Check matrix structure
+        // lcp[i][j] = lcp[j][i]
+        // lcp[i][i] = n - i
+        // lcp[i][j] > 0: lcp[i+1][j+1] =  lcp[i][j] - 1
+        // lcp[i][j] < n - i, lcp[i][j] < n - j
+        for (int i = 0; i < n; i++) {
+            if (lcp[i][i] != n - i) return "";
+            for (int j = 0; j < n; j++) {
+                if (lcp[i][j] != lcp[j][i]) return "";
+                if (lcp[i][j] > min(n - i, n - j)) return ""; 
+                if (i + 1 < n && j + 1 < n && lcp[i][j] > 0) {
+                    if (lcp[i+1][j+1] + 1 != lcp[i][j]) return "";
+                }
             }
         }
 
-        return ans;
+        vector<int> result(n, -1);
+        vector<vector<int>> isOK(n, vector<int>(26, 1));
+        for (int i = 0; i < n; i++) {
+            if (result[i] == -1) {
+                for (int j = 0; j < 26; j++) {
+                    if (isOK[i][j]) {
+                        result[i] = j;
+                        break;
+                    }
+                }
+                if (result[i] == -1) return "";
+            }
+            // Check and populate
+            for (int j = i+1; j < n; j++) {
+                if (lcp[i][j] == 0) {
+                    // conflict with existing letter
+                    if (result[j] >= 0 && result[j] == result[i]) return "";
+                    isOK[j][result[i]] = false;
+                } else {
+                    // conflict with existing letter
+                    if (result[j] >= 0 && result[j] != result[i]) return "";
+                    // conflict with can not fill
+                    if (result[j] == -1 && !isOK[j][result[i]]) return "";
+                    result[j] = result[i];
+                }
+            }
+        }
+        string s;
+        for (int c: result) s += char(c+'a');
+        return s;
     }
 };
