43d91f5556d318b19bf3c612b3883e7e336a675f
Neha <118845830+neha2k4@users.noreply.github.com>
2025-03-27 23:24:12 +0530

    Time: 0 ms (100%), Space: 89.9 MB (85.66%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
@@ -1,40 +1,20 @@
-/* Approach : Majority Element (Boyer - Moore)
-1. Find majority element using Moore Voting Algorithm
-2. Try split at each point and check if it is majority in both half
-Time : O(n)
-Space : O(1)
-*/
-
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        int n = nums.size();
-        //find the majority element using Moore's voting algorithm
-        int majority = nums[0], count = 1;
-        for (int i=1; i<n; i++){
-            if(nums[i] == majority)
-                count++;
-            else
-                count--;
-            if(count == 0){
-                majority = nums[i];
-                count = 1;
-            }
+        const int n=nums.size();
+        //Boyer–Moore majority vote algo
+        int cnt=0, xM=0;
+        for(int x: nums){
+            if (cnt==0) xM=x;
+            cnt+=(x==xM)?1:-1;
         }
-    
-        //get frequency of majority element
-        int majorityCount = 0;
-        for(auto num: nums)
-            if(num == majority)
-                majorityCount++;
-    
-        // traverse array and check if split is possible at this point or not
-        int leftCount = 0;
+        //Count maxF
+        int maxF=count(nums.begin(), nums.end(), xM);
+        // find the valid split
+        int cntxM=0;
         for(int i=0; i<n; i++){
-            if(nums[i] == majority)
-                leftCount++;
-            // check if it is majority in both half 
-            if((leftCount*2 > i+1)  && ((majorityCount - leftCount)*2 > n-i-1)) 
+            cntxM+=nums[i]==xM;
+            if (cntxM*2>(i+1) && ((maxF-cntxM)*2>(n-i-1)))
                 return i;
         }
         return -1;
